4e92923a85217323bd2adef093b9c276b16dea82
Alexa B <alxa@google.com>
2020-08-06 17:12:17 -0400

    docs: Generate using new common.py_samples() synthtool functionality

--- synth.py
+++ synth.py
@@ -27,76 +27,77 @@ gapic = gcp.GAPICBazel()
 common = gcp.CommonTemplates()
 version = "v1"
 
-# ----------------------------------------------------------------------------
-# Generate kms GAPIC layer
-# ----------------------------------------------------------------------------
-library = gapic.py_library(
-    service="kms",
-    version=version,
-    bazel_target="//google/cloud/kms/v1:kms-v1-py",
-    include_protos=True,
-)
+# # ----------------------------------------------------------------------------
+# # Generate kms GAPIC layer
+# # ----------------------------------------------------------------------------
+# library = gapic.py_library(
+#     service="kms",
+#     version=version,
+#     bazel_target="//google/cloud/kms/v1:kms-v1-py",
+#     include_protos=True,
+# )
 
-s.move(library, excludes=["README.rst", "setup.py", "nox*.py", "docs/index.rst"])
+# s.move(library, excludes=["README.rst", "setup.py", "nox*.py", "docs/index.rst"])
 
-# Escape single '_' which RST treats as target names
-s.replace("google/**/resources.py", '''"(.*?)_((SIGN)|(DECRYPT))_"''', '''"\g<1>_\g<2>\_"''')
+# # Escape single '_' which RST treats as target names
+# s.replace("google/**/resources.py", '''"(.*?)_((SIGN)|(DECRYPT))_"''', '''"\g<1>_\g<2>\_"''')
 
-# Docstrings of *_iam_policy() methods are formatted poorly and must be fixed
-# in order to avoid docstring format warnings in docs.
-s.replace(
-    "google/**/*client.py",
-    r"(\s+)Args:",
-    "\n\g<1>Args:"
-)
-s.replace(
-    "google/**/*client.py",
-    r"(\s+)\*\*JSON Example\*\*\s+::",
-    "\n\g<1>**JSON Example**::\n",
-)
-s.replace(
-    "google/**/*client.py",
-    r"(\s+)\*\*YAML Example\*\*\s+::",
-    "\n\g<1>**YAML Example**::\n",
-)
-s.replace(
-    "google/**/*client.py",
-    r"(\s+)For a description of IAM and its features, see",
-    "\n\g<0>",
-)
+# # Docstrings of *_iam_policy() methods are formatted poorly and must be fixed
+# # in order to avoid docstring format warnings in docs.
+# s.replace(
+#     "google/**/*client.py",
+#     r"(\s+)Args:",
+#     "\n\g<1>Args:"
+# )
+# s.replace(
+#     "google/**/*client.py",
+#     r"(\s+)\*\*JSON Example\*\*\s+::",
+#     "\n\g<1>**JSON Example**::\n",
+# )
+# s.replace(
+#     "google/**/*client.py",
+#     r"(\s+)\*\*YAML Example\*\*\s+::",
+#     "\n\g<1>**YAML Example**::\n",
+# )
+# s.replace(
+#     "google/**/*client.py",
+#     r"(\s+)For a description of IAM and its features, see",
+#     "\n\g<0>",
+# )
 
-# TODO(busunkim): Remove once https://github.com/googleapis/gapic-generator-python/pull/555 is merged
-s.replace(
-    "google/**/base.py",
-    """from google\.api_core import gapic_v1\s+# type: ignore""",
-    """from google.api_core import gapic_v1  # type: ignore
-from google.api_core import retry as retries  # type: ignore""",
-)
+# # TODO(busunkim): Remove once https://github.com/googleapis/gapic-generator-python/pull/555 is merged
+# s.replace(
+#     "google/**/base.py",
+#     """from google\.api_core import gapic_v1\s+# type: ignore""",
+#     """from google.api_core import gapic_v1  # type: ignore
+# from google.api_core import retry as retries  # type: ignore""",
+# )
 
-# fix bad indentation
-s.replace(
-    "google/**/*service.py",
-    """(\s+)settings resource.
-\s+If empty all mutable fields will be updated.""",
-    """\g<1>settings resource.
-\g<1>If empty all mutable fields will be updated.""",
-)
+# # fix bad indentation
+# s.replace(
+#     "google/**/*service.py",
+#     """(\s+)settings resource.
+# \s+If empty all mutable fields will be updated.""",
+#     """\g<1>settings resource.
+# \g<1>If empty all mutable fields will be updated.""",
+# )
 
 
-# ----------------------------------------------------------------------------
-# Add templated files
-# ----------------------------------------------------------------------------
-templated_files = common.py_library(
-    cov_level=99,
-    samples=True,
-    microgenerator=True,
-)
-s.move(templated_files, excludes=[".coveragerc"])  # microgenerator has a good .coveragerc file
+# # ----------------------------------------------------------------------------
+# # Add templated files
+# # ----------------------------------------------------------------------------
+# templated_files = common.py_library(
+#     cov_level=99,
+#     samples=True,
+#     microgenerator=True,
+# )
+# s.move(templated_files, excludes=[".coveragerc"])  # microgenerator has a good .coveragerc file
 
-# ----------------------------------------------------------------------------
-# Samples templates
-# ----------------------------------------------------------------------------
+# # ----------------------------------------------------------------------------
+# # Samples templates
+# # ----------------------------------------------------------------------------
 
-python.py_samples()
+sample_files = common.py_samples(unit_cov_level=97, cov_level=99, samples=True)
+s.move(sample_files, excludes=['noxfile.py'])
 
-s.shell.run(["nox", "-s", "blacken"], hide_output=False)
+# s.shell.run(["nox", "-s", "blacken"], hide_output=False)
