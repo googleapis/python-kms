ee56b46af3ba2c347b5bb69c518bde1ba0284206
yoshi-automation <yoshi-automation@google.com>
2020-08-26 06:39:58 -0700

    chore(java): default samples/snippets pom should not use libraries-bom
    We are now only adding clients to libraries-bom once the client is
    1.0.0. Since this template is created at repository creation time, we
    don't want to include the libraries-bom installation method here.
    
    Source-Author: Jeff Ching <chingor@google.com>
    Source-Date: Fri Jul 24 15:03:17 2020 -0700
    Source-Repo: googleapis/synthtool
    Source-Sha: 5506723fef367557d83cf072d0c18020c049fcb8
    Source-Link: https://github.com/googleapis/synthtool/commit/5506723fef367557d83cf072d0c18020c049fcb8

--- tests/unit/gapic/kms_v1/test_key_management_service.py
+++ tests/unit/gapic/kms_v1/test_key_management_service.py
@@ -6289,35 +6289,6 @@ def test_key_management_service_grpc_asyncio_transport_channel_mtls_with_adc(
         assert transport.grpc_channel == mock_grpc_channel
 
 
-def test_import_job_path():
-    project = "squid"
-    location = "clam"
-    key_ring = "whelk"
-    import_job = "octopus"
-
-    expected = "projects/{project}/locations/{location}/keyRings/{key_ring}/importJobs/{import_job}".format(
-        project=project, location=location, key_ring=key_ring, import_job=import_job,
-    )
-    actual = KeyManagementServiceClient.import_job_path(
-        project, location, key_ring, import_job
-    )
-    assert expected == actual
-
-
-def test_parse_import_job_path():
-    expected = {
-        "project": "oyster",
-        "location": "nudibranch",
-        "key_ring": "cuttlefish",
-        "import_job": "mussel",
-    }
-    path = KeyManagementServiceClient.import_job_path(**expected)
-
-    # Check that the path construction is reversible.
-    actual = KeyManagementServiceClient.parse_import_job_path(path)
-    assert expected == actual
-
-
 def test_crypto_key_version_path():
     project = "squid"
     location = "clam"
@@ -6353,28 +6324,32 @@ def test_parse_crypto_key_version_path():
     assert expected == actual
 
 
-def test_key_ring_path():
+def test_import_job_path():
     project = "squid"
     location = "clam"
     key_ring = "whelk"
+    import_job = "octopus"
 
-    expected = "projects/{project}/locations/{location}/keyRings/{key_ring}".format(
-        project=project, location=location, key_ring=key_ring,
+    expected = "projects/{project}/locations/{location}/keyRings/{key_ring}/importJobs/{import_job}".format(
+        project=project, location=location, key_ring=key_ring, import_job=import_job,
     )
-    actual = KeyManagementServiceClient.key_ring_path(project, location, key_ring)
+    actual = KeyManagementServiceClient.import_job_path(
+        project, location, key_ring, import_job
+    )
     assert expected == actual
 
 
-def test_parse_key_ring_path():
+def test_parse_import_job_path():
     expected = {
-        "project": "octopus",
-        "location": "oyster",
-        "key_ring": "nudibranch",
+        "project": "oyster",
+        "location": "nudibranch",
+        "key_ring": "cuttlefish",
+        "import_job": "mussel",
     }
-    path = KeyManagementServiceClient.key_ring_path(**expected)
+    path = KeyManagementServiceClient.import_job_path(**expected)
 
     # Check that the path construction is reversible.
-    actual = KeyManagementServiceClient.parse_key_ring_path(path)
+    actual = KeyManagementServiceClient.parse_import_job_path(path)
     assert expected == actual
 
 
@@ -6404,6 +6379,31 @@ def test_parse_crypto_key_path():
 
     # Check that the path construction is reversible.
     actual = KeyManagementServiceClient.parse_crypto_key_path(path)
+    assert expected == actual
+
+
+def test_key_ring_path():
+    project = "squid"
+    location = "clam"
+    key_ring = "whelk"
+
+    expected = "projects/{project}/locations/{location}/keyRings/{key_ring}".format(
+        project=project, location=location, key_ring=key_ring,
+    )
+    actual = KeyManagementServiceClient.key_ring_path(project, location, key_ring)
+    assert expected == actual
+
+
+def test_parse_key_ring_path():
+    expected = {
+        "project": "octopus",
+        "location": "oyster",
+        "key_ring": "nudibranch",
+    }
+    path = KeyManagementServiceClient.key_ring_path(**expected)
+
+    # Check that the path construction is reversible.
+    actual = KeyManagementServiceClient.parse_key_ring_path(path)
     assert expected == actual
 
 
