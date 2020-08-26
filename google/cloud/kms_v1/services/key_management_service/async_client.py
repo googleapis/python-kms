acc90010baf2a729dd87164953932fa6799002b1
yoshi-automation <yoshi-automation@google.com>
2020-08-26 06:52:38 -0700

    feat: Move datacatalog API to Python Microgenerator
    PiperOrigin-RevId: 326481100
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Thu Aug 13 10:58:09 2020 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: d2641bdeed624c19bc720f59a5df12379d8220b9
    Source-Link: https://github.com/googleapis/googleapis/commit/d2641bdeed624c19bc720f59a5df12379d8220b9

--- google/cloud/kms_v1/services/key_management_service/async_client.py
+++ google/cloud/kms_v1/services/key_management_service/async_client.py
@@ -63,15 +63,15 @@ class KeyManagementServiceAsyncClient:
     DEFAULT_ENDPOINT = KeyManagementServiceClient.DEFAULT_ENDPOINT
     DEFAULT_MTLS_ENDPOINT = KeyManagementServiceClient.DEFAULT_MTLS_ENDPOINT
 
-    crypto_key_path = staticmethod(KeyManagementServiceClient.crypto_key_path)
+    key_ring_path = staticmethod(KeyManagementServiceClient.key_ring_path)
 
     crypto_key_version_path = staticmethod(
         KeyManagementServiceClient.crypto_key_version_path
     )
 
-    key_ring_path = staticmethod(KeyManagementServiceClient.key_ring_path)
-
     import_job_path = staticmethod(KeyManagementServiceClient.import_job_path)
+
+    crypto_key_path = staticmethod(KeyManagementServiceClient.crypto_key_path)
 
     from_service_account_file = KeyManagementServiceClient.from_service_account_file
     from_service_account_json = from_service_account_file
